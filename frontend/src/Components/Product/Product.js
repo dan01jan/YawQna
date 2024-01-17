b43746b7a87eae1b23f49a29d2d9a4a6f0947b3c
dan01jan <danize1001@gmail.com>
2024-01-17 16:27:26 +0800

    No URL Error

--- frontend/src/Components/Product/Product.js
+++ frontend/src/Components/Product/Product.js
@@ -5,10 +5,9 @@ const Product = ({ product }) => {
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="card p-3 rounded">
-                <img
-                    className="card-img-top mx-auto"
-                    src={product.images[0].url}
-                />
+            {product.images && product.images.length > 0 && (
+                <img className="card-img-top mx-auto" src={product.images[0].url} alt={product.name} />
+            )}
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">
                         <a href="">{product.name}</a>
